c4b30018173298d329cec63022288d7217f13af8
engsfjvolfe <engsfjvolfe@gmail.com>
2022-09-09 09:50:00 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,16 +1,16 @@
 import icon from '../../assets/img/notification-icon.svg'
 import { BASE_URL } from '../../utils/request'
 import './styles.css'
-import axios from 'axios';
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 type Props = {
   saleId: number
 }
 
 function handleClick(id: number) {
-  axios(`${BASE_URL}/sales/${id}/notification`)
-  .then(response => {
-    console.log("SUCESSO")
+  axios(`${BASE_URL}/sales/${id}/notification`).then((response) => {
+    toast.info('SMS enviado com sucesso!')
   })
 }
 
